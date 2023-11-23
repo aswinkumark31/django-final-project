2630eb219618789cfe9831f99cc5107b3ef3fca0
aswin <aswinsynnefo3132001@gmail.com>
2023-11-23 19:00:01 +0530

    site adding

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('about/',views.about,name='about'),
+    path('package/',views.package,name='package'),
+    path('department/',views.department,name='department'),
+    path('blog/',views.blog,name='blog'),
+    path('gallery/',views.gallery,name='gallery'),
+    path('contact/',views.contact,name='contact'),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -8,4 +8,21 @@ def index(request):
     pro=models.Products.objects.all()
     return render(request,'index.html',{"pro":pro})
 
+def about(request):
+    return render(request,'about.html')
+
+def package(request):
+    return render(request,'package.html')
+
+def department(request):
+    return render(request,'department.html')
+
+def blog(request):
+    return render(request,'blog.html')
+
+def gallery(request):
+    return render(request,'gallery.html')
+
+def contact(request):
+    return render(request,'contact.html')
 
